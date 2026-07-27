4f435aa8fa338a6e3f13b61983ca89a349a678c6
blob <130174758+BlobCybersecurity@users.noreply.github.com>
2026-07-27 14:05:21 +0200

    Add comments instructions for indexes module.

---
+++ lib/helpers/indexes_module.rb
@@ -0,0 +1,14 @@
+module Indexes
+  def index_of_ones(computer_guess, algorithm_array)
+  end
+
+  def index_of_threes(computer_guess, algorithm_array)
+  end
+end
+
+# TODO
+#
+# Make this so that we can use the conditional method for the specific filter to use (either on the threes or change the twos positions in computer pyaer )
+# should just give the indexes of the threes and the ones
+# Reason:
+# We want to avoid making a new position that is already occupied by 1 or 3 because we only want to move the  2's
